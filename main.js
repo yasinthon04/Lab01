2135b34dcd88013242aabcdba68bd13d94c82c9a
Yasinthon Roekkamyee <yasinthon_r@cmu.ac.th>
2022-06-30 23:17:49 +0700

    6.7

--- main.js
+++ main.js
@@ -23,6 +23,16 @@ const app = Vue.createApp({
         },
         updateImage(variantImage){
             this.image = variantImage
-        },
+        }, 
+        changeStatus() {
+            this.count += 1
+            if(this.count%2 !== 0){
+                this.inventory = 0
+            }
+            else if(this.count%2 === 0) {
+                this.inventory = 100
+            }
+
+        }
     }
 })
