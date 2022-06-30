7a677f1f1f87b33565ac3de016c9a807ea9176d9
Yasinthon Roekkamyee <yasinthon_r@cmu.ac.th>
2022-06-30 23:12:13 +0700

    5.4

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-           product : 'Shoes',
-           info : 'Welcome to our store.',
-           image: './assets/images/socks_green.jpg',
-           web: 'https://www.camt.cmu.ac.th/index.php/th/',
-           inStock: true,
-           inventory: 100,
-           details: ['50% cotton' , '30% wool','20% polyester ' ],
-           variants : [
-            { id: 2234 , color:'green'},
-            { id: 2235 , color:'blue'}
+            product: 'Shoes',
+            info: 'Welcome to our store.',
+            image: './assets/images/socks_green.jpg',
+            web: 'https://www.camt.cmu.ac.th/index.php/th/',
+            inStock: true,
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% polyester '],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
 
-        ]
+            ],
+            size: ['Size = S,M,L'],
         }
     }
 })
