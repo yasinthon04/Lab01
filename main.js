122e48cf5eff6769e718ba335026f8fe60bf2214
Yasinthon Roekkamyee <yasinthon_r@cmu.ac.th>
2022-06-30 23:10:00 +0700

    5.3

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const app = Vue.createApp({
            inStock: true,
            inventory: 100,
            details: ['50% cotton' , '30% wool','20% polyester ' ],
+           variants : [
+            { id: 2234 , color:'green'},
+            { id: 2235 , color:'blue'}
+
+        ]
         }
     }
 })
