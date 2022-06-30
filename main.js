846d5e422c0a9d0256d815dea605f6d9f6af8309
Yasinthon Roekkamyee <yasinthon_r@cmu.ac.th>
2022-06-30 22:21:25 +0700

    3.2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
            product : 'Boots',
-           info : 'Welcome to our store.'
+           info : 'Welcome to our store.',
+           image: './assets/images/socks_green.jpg'
         }
     }
 })
