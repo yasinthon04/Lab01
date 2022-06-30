20d9b06f07527f7d3e5910f6c503eee77d8aae41
Yasinthon Roekkamyee <yasinthon_r@cmu.ac.th>
2022-06-30 23:15:32 +0700

    6.6

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester '],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234 , color:'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235 , color:'blue', image: './assets/images/socks_blue.jpg'}
 
             ],
             size: ['Size = S,M,L'],
